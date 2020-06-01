98c6cdb5cac8551d5e37ef510fa277c34bb1c9d4
Daniel V <daniel.v.veronez@gmail.com>
2020-06-01 19:31:38 -0300

    Feito os exercicios

--- aula05/backend/src/controllers/userController.js
+++ aula05/backend/src/controllers/userController.js
@@ -4,8 +4,30 @@ const User      = mongoose.model('User');
 
 module.exports = {
     async persisteUser(req,res){
-        const payload = await req.body;
-        User.create(payload);
+        const {userGit} = req.body;
+        //Usar a crase para fazer esta linha abaixo
+        const response = await axios.get(`https://api.github.com/users/${userGit}`);
+        //descontrução de variável
+        const {
+            login, 
+            name, 
+            avatar_url, 
+            companu, 
+            public_repos, 
+            followers, 
+            bio
+        } = response.data;
+        
+        const payload = await User.create({
+            login, 
+            name, 
+            avatar_url, 
+            companu, 
+            public_repos, 
+            followers, 
+            bio
+        });
+
         res.json(payload);
     },
     async listUser(req,res){
@@ -15,5 +37,17 @@ module.exports = {
     async getUserGit(req,res){
         const response = await axios.get('https://api.github.com/users/leonardogandrade')
         res.json(response.data);
+    },
+    //usuários antes de 2020
+    async listDate(req, res){
+        const response = await axios.get('https://api.github.com/users/leonardogandrade')
+        const {login, created_at} = response.data;
+        const created_at_date = new Date(Date.parse(created_at));
+
+        if(created_at.getFullYear() < 2020){
+            res.json("Usuário antigo");
+        }else{
+            res.json("Usuário novo");
+        }
     }
 }
--- aula05/backend/src/models/userModel.js
+++ aula05/backend/src/models/userModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     login : String,
     name : String,
-    biografia : String,
+    bio : String,
     avatar_url : String,
     company : String,
     public_repos : Number,
